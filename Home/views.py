3a35ea9c2743291785fa62d5f35903e41a2767c7
Kareem <kimo.h2010@gmail.com>
2016-12-28 21:47:55 +0200

    fixed issues

--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from NeuralNetwork.deep_learner.train import *
+from NeuralNetwork.deep_learner.featureVectorization import *
 
 def index(request):
-    return HttpResponse("Hello world")
+    return HttpResponse("Hello World")
